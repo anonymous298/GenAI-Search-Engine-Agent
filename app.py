76c2bf429ff56f9700e15a6473d3bda3a416fa4f
anonymous298 <tackletalha@gmail.com>
2025-02-20 21:13:04 +0500

    creating agent

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 # Importing Necessary Dependencies
+import modulefinder
 import os
 import streamlit as st
 
@@ -13,6 +14,54 @@ from langchain.agents import  AgentExecutor, initialize_agent, AgentType
 from langchain_community.chat_message_histories import ChatMessageHistory
 from langchain_core.chat_history import BaseChatMessageHistory
 from langchain_core.runnables.history import RunnableWithMessageHistory
+from langchain.callbacks import StreamlitCallbackHandler
 
+# Building Tools for our agents
+def get_tools():
+    '''
+    This function will build tools for our agents.
+    '''
+    
+    try:
+        # Building our Arxiv Tool
+        arxiv_wrapper = ArxivAPIWrapper(top_k_results=1, doc_content_chars_max=200)
+        arxiv_tool = ArxivQueryRun(api_wrapper = arxiv_wrapper)
 
+        # Building our Wikipedia Tool
+        wiki_wrapper = WikipediaAPIWrapper(top_k_results=1, doc_content_chars_max=200)
+        wiki_tool = WikipediaQueryRun(api_wrapper=wiki_wrapper)
 
+        # Building our DuckDuckGoSearch Tool
+        search = DuckDuckGoSearchRun(name='Search')
+
+        # Returing the tools
+        return (
+            arxiv_tool,
+            wiki_tool,
+            search
+        )
+
+    except Exception as e:
+        print(e)
+
+# Building our Streamlit Application
+st.title('Basic Search-Engine Agent Using Langchain')
+
+def initialize_sessions():
+    '''
+    This will initialize all the steps of building the agents in session so it will not exectue again and again.
+    '''
+
+    st.session_state.arxiv_tool, st.session_state.wiki_tool, st.session_state.search_tool = get_tools()
+    st.session_state.tools = [st.session_state.arxiv_tool, st.session_state.wiki_tool, st.session_state.search_tool]
+
+    st.session_state.llm = ChatOllama(
+        model = 'llama3.2:3b'
+    )
+
+    st.session_state.agent = initialize_agent(
+        llm = st.session_state.llm,
+        tools = st.session_state.tools,
+        agent = AgentType.ZERO_SHOT_REACT_DESCRIPTION,
+        verbose = True
+    )
